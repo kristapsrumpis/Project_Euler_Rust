0870728f028890b062868eaa729353de48da3605
Kristaps Rumpis <kristapsrumpis.kr@gmail.com>
2026-03-10 09:29:54 +0000

    add first euler problem from mod as reusable function and set executin speed chek

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::time::Instant; // import time module for script speed test
+mod _01; // Euler problem 01
+use _01::multiples_sum; //declears wich function use from  mod file
+
 fn main() {
-    println!("Hello, world!");
+    let start_01 = Instant::now(); // set start point for timer for Euler problem 01
+
+    // let sum_of_10 = multiples_sum(10); // run script for test multiple bellow 10
+    // println!("Sum of multibles below 10: {}", sum_of_10);
+
+    let sum_of_1000 = multiples_sum(1000); // Run script to get sum of multiples below 1000
+    println!("Euler problem 01: {}", sum_of_1000);
+
+    // gets and print in console script duration time for euler problem 01
+    let duration_01 = start_01.elapsed();
+    println!("Time: {:?}", duration_01);
 }
